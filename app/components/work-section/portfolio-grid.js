9c7ca37b081e5c7fe0f289d5b5fac3ef47151fd1
Kyle Tong <kyle.simon.tong@gmail.com>
2023-02-10 21:52:23 +0900

    Add portfolio grid skeleton

---
+++ app/components/work-section/portfolio-grid.js
@@ -0,0 +1,35 @@
+import Component from '@glimmer/component';
+
+export default class WorkSectionPortfolioGridComponent extends Component {
+  portfolioData = [
+    {
+      title: 'Realestate.co.nz',
+      subtitle: 'Full-stack development',
+      mainImagePath: '',
+      // TODO: Make secondary images an array for more details.
+      secondaryImagePath: '',
+      description: `We implemented the front-end real estate listing displayed here
+                    from a provided design.This gave the agents all data needed to give their clients meaningful insights.We leveraged data captured
+                    in Google Analytics via Elasticsearch to provide real time reports to agents wanting this data.`,
+    },
+    {
+      title: 'Entrance Design',
+      subtitle: 'Full-stack development',
+      mainImagePath: '',
+      secondaryImagePath: '',
+      description: `We took a design from the client, implemented it, and then lifted and shifted infrastructure into AWS. We also
+                    moved their website over to a single- page application for faster performance and modern security.
+                    We host and support all of their infrastructure to ensure the site runs smoothly.`,
+    },
+    {
+      title: 'Realestate.co.nz',
+      subtitle: 'Backend Development',
+      mainImagePath: '',
+      secondaryImagePath: '',
+      description: `We took data from a legacy platform and manipulated it to allow a customer service team to
+                    input location data and define new areas based on points on a Google Maps instance.We then convert this to a
+                    format the platform understands and export to the main application.This involved lots of heavy data lifting and manipulation,
+                    as well as making sure the infrastructure could handle the load.`,
+    },
+  ];
+}
