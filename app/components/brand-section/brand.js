829b0e1f2c5446a7f1c83fe830a90832df89ddd3
Kyle Tong <kyle.simon.tong@gmail.com>
2023-02-11 09:40:02 +0900

    Add brand section right quick

--- app/components/brand-section/brand.js
+++ app/components/brand-section/brand.js
@@ -1,3 +1,16 @@
 import Component from '@glimmer/component';
 
-export default class BrandSectionBrandComponent extends Component {}
+export default class BrandSectionBrandComponent extends Component {
+  brandData = [
+    {
+      brandName: 'Entrance Design',
+      brandUrl: 'https://www.entrancedesign.co.nz',
+      brandImagePath: 'assets/img/client-logo-1.webp',
+    },
+    {
+      brandName: 'realestate.co.nz',
+      brandUrl: 'https://www.realestate.co.nz',
+      brandImagePath: 'assets/img/client-logo-2.webp',
+    },
+  ];
+}
