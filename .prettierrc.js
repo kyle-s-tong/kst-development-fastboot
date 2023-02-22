ff07d040dc0114c99062d24ab959c2346811b806
Kyle Tong <kyle.simon.tong@gmail.com>
2023-02-22 16:53:40 +0900

    Fix class orders using new plugin

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = {
+  tailwindConfig: './tailwind.config.js',
+  plugins: [require('prettier-plugin-tailwindcss')],
   overrides: [
     {
       files: '*.{js,ts}',
