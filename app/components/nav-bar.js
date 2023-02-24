036578b3ff23cb15eb9375a6e53d30b20c766ba6
Kyle Tong <kyle.simon.tong@gmail.com>
2023-02-24 16:31:57 +0900

    Add navbar but don't build it yet

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
 
-// TODO: implement logic
 // eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class WorkSectionPortfolioModalComponent extends Component {}
+export default class NavBarComponent extends Component {}
