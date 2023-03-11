3c2658644e6ec34cf8e24363bdb7f49f1600ecf4
Kyle Tong <kyle.simon.tong@gmail.com>
2023-03-11 16:30:38 +0900

    Revert changes after figuring out it was due to http (no s)

--- srv/app.js
+++ srv/app.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-undef
 const FastBootAppServer = require('fastboot-app-server');
 
 const distPath = 'dist';
 
 const options = {
   distPath,
+  gzip: true, // Optional - Enables gzip compression.
+  host: '0.0.0.0', // Optional - Sets the host the server listens on.
 };
 
 const server = new FastBootAppServer({ ...options });
