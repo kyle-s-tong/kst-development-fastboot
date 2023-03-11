61e00464599c97d39c9cf42ba2409452eaf533e7
Kyle Tong <kyle.simon.tong@gmail.com>
2023-03-11 17:25:39 +0900

    Add eslint and templating fixes

--- app/components/footer-section/footer.js
+++ app/components/footer-section/footer.js
@@ -1,3 +1,5 @@
 import Component from '@glimmer/component';
 
+// TODO: Add button to go back to the top
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes
 export default class FooterSectionFooterComponent extends Component {}
