be74b846cf28d45b3feae2f4e255a5761c709e6b
Kyle Tong <kyle.simon.tong@gmail.com>
2023-03-11 15:57:08 +0900

    Playing with app.js for deployment

--- srv/app.js
+++ srv/app.js
@@ -4,8 +4,6 @@ const distPath = 'dist';
 
 const options = {
   distPath,
-  gzip: true, // Optional - Enables gzip compression.
-  host: '0.0.0.0', // Optional - Sets the host the server listens on.
 };
 
 const server = new FastBootAppServer({ ...options });
