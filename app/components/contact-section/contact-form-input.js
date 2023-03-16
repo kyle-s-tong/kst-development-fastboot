d00677a465cfb685a93d757cad61fb7451c564f0
Kyle Tong <kyle.simon.tong@gmail.com>
2023-03-16 17:07:41 +0900

    Add placeholder calculation for compulsory fields

--- app/components/contact-section/contact-form-input.js
+++ app/components/contact-section/contact-form-input.js
@@ -2,4 +2,14 @@ import Component from '@glimmer/component';
 
 // TODO: Add validation rules
 // eslint-disable-next-line ember/no-empty-glimmer-component-classes
-export default class ContactSectionContactFormInputComponent extends Component {}
+export default class ContactSectionContactFormInputComponent extends Component {
+  get placeholder() {
+    const name = this.args.elementName;
+    const capitalisedName = name.charAt(0).toUpperCase() + name.slice(1);
+
+    if (this.args.isRequiredField) {
+      return `*${capitalisedName}:`;
+    }
+    return `${capitalisedName}:`;
+  }
+}
