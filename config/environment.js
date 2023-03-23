08a0b147ecb8a5bea95faed04370581ec3d4f248
Kyle Tong <kyle.simon.tong@gmail.com>
2023-03-23 21:48:32 +0900

    Get host env variable working for the CMS hook up

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,7 @@ module.exports = function (environment) {
   };
 
   if (environment === 'development') {
+    ENV.APP.cmsHost = 'localhost:8080';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
